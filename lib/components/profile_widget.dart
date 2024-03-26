17b87d2f9a53772a03e21c66a121bbf25577ca29
heavenman9761 <dn9318dn@gmail.com>
2024-03-26 17:59:06 +0900

    2024-03-26 02 from Windows

--- lib/components/profile_widget.dart
+++ lib/components/profile_widget.dart
@@ -102,6 +102,29 @@ class ProfileWidget extends ConsumerWidget{
               ),
             )
         ),
+        Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+            child: Container(
+              color: Colors.white,
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("  공유자 관리", style: TextStyle(fontSize: 20),),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text("공유자 추가", style: TextStyle(fontSize: 18, color: Colors.grey)),
+                      IconButton(
+                        icon: Icon(Icons.chevron_right),
+                        onPressed: null,
+                      )
+                    ],
+                  )
+
+                ],
+              ),
+            )
+        ),
         const SizedBox(height: 10),
         Padding(
             padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
