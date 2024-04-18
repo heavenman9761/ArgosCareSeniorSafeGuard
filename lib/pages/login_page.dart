19a8fc2ce3b57c57410bba268ba7cf64dd2a2cf8
heavenman9761 <dn9318dn@gmail.com>
2024-04-18 18:16:09 +0900

    2024-04-18 02 from Windows

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -200,129 +200,156 @@ class _LoginPageState extends State<LoginPage> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const SizedBox(height: 50),
-
-                  // logo
-                  const Icon(
-                    Icons.lock,
-                    size: 100,
-                  ),
-
-                  const SizedBox(height: 30),
-
-                  // welcome back, you've been missed!
-                  Text(
-                    'Welcome back you\'ve been missed!',
-                    style: TextStyle(
-                      color: Colors.grey[700],
-                      fontSize: 16,
-                    ),
-                  ),
-
-                  const SizedBox(height: 25),
-
-                  renderTextFormField(
-                    context: context,
-                    label: '메일 주소',
-                    keyNumber: 1,
-                    icon: const Icon(Icons.mail, color: Colors.grey,),
-                    suffixIcon: _mailController.text.isNotEmpty ?
-                    IconButton(
-                      icon: const Icon(Icons.clear),
-                      onPressed: () {
-                        _mailController.clear();
-                        setState(() { });
-                      },
-                    ) : null,
-                    controller: _mailController,
-                    keyboardType: TextInputType.emailAddress,
-                    // initialValue: 'dn9318dn@gmail.com',
-                    onChanged: (val) {
-                      setState(() { });
-                    },
-                    onSaved: (val) {
-                      setState(() {
-                        email = val;
-                      });
-
-                    },
-                    validator: (val) {
-                      if (val.length < 1) {
-                        return '이메일은 필수사항입니다.';
-                      }
-                      return (val as String).isValidEmailFormat() ? null : '이메일 형식이 아닙니다.';
-                    },
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  renderTextFormField(
-                    context: context,
-                    label: '비밀 번호',
-                    keyNumber: 2,
-                    icon: const Icon(Icons.lock, color: Colors.grey,),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        passwordVisible ? Icons.visibility : Icons.visibility_off,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          passwordVisible = !passwordVisible;
-                        });
-                      },
-                    ),
-                    keyboardType: TextInputType.text,
-                    obscureText: passwordVisible,
-                    initialValue: '121212',
-                    onSaved: (val) {
-                      setState(() {
-                        password = val;
-                      });
-
-                    },
-                    validator: (val) {
-                      if (val.length < 6) {
-                        return '비밀번호는 6글자 이상 12글자 이하로 입력 해주셔야합니다.';
-                      }
-
-                      //return (val as String).isValidPasswordFormatType1() ? null : '비밀번호는 영문(소문자, 대문자), 숫자, 특수문자로 이루어진 6 ~ 12 자리입니다.';
-                      return (val as String).isValidOnlyNumber() ? null : '비밀번호는 숫자로 이루어진 6 ~ 12 자리입니다.';
-                    },
-                  ),
-
-                  const SizedBox(height: 10),
-
-                  // forgot password?
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          'Forgot Password?',
-                          style: TextStyle(color: Colors.grey[600]),
-                        ),
-                      ],
-                    ),
-                  ),
-
-                  const SizedBox(height: 25),
-
-                  // sign in button
-                  loginWidget(context),
-                  // Expanded(child: SizedBox.shrink()),
-                  // Spacer(),
-                  Expanded(
-                    child: Container(
-                      // color: Colors.yellow,
-                      width: double.infinity,
-                      height: double.infinity,
-                      alignment: Alignment.bottomCenter,
+                  Flexible(
+                      fit: FlexFit.tight,
+                      flex: 2,
                       child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 50),
+
+                          // logo
+                          const Icon(
+                            Icons.lock,
+                            size: 100,
+                          ),
+
+                          const SizedBox(height: 30),
+
+                          // welcome back, you've been missed!
+                          Text(
+                            'Welcome back you\'ve been missed!',
+                            style: TextStyle(
+                              color: Colors.grey[700],
+                              fontSize: 16,
+                            ),
+                          ),
+
+                          const SizedBox(height: 18),
+                        ],
+                      )
+                  ),
+                  Flexible(
+                      fit: FlexFit.tight,
+                      flex: 2,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          renderTextFormField(
+                            context: context,
+                            label: '메일 주소',
+                            keyNumber: 1,
+                            icon: const Icon(Icons.mail, color: Colors.grey,),
+                            suffixIcon: _mailController.text.isNotEmpty ?
+                            IconButton(
+                              icon: const Icon(Icons.clear),
+                              onPressed: () {
+                                _mailController.clear();
+                                setState(() { });
+                              },
+                            ) : null,
+                            controller: _mailController,
+                            keyboardType: TextInputType.emailAddress,
+                            // initialValue: 'dn9318dn@gmail.com',
+                            onChanged: (val) {
+                              setState(() { });
+                            },
+                            onSaved: (val) {
+                              setState(() {
+                                email = val;
+                              });
+
+                            },
+                            validator: (val) {
+                              if (val.length < 1) {
+                                return '이메일은 필수사항입니다.';
+                              }
+                              return (val as String).isValidEmailFormat() ? null : '이메일 형식이 아닙니다.';
+                            },
+                          ),
+
+                          const SizedBox(height: 10),
+
+                          renderTextFormField(
+                            context: context,
+                            label: '비밀 번호',
+                            keyNumber: 2,
+                            icon: const Icon(Icons.lock, color: Colors.grey,),
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                passwordVisible ? Icons.visibility : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  passwordVisible = !passwordVisible;
+                                });
+                              },
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: passwordVisible,
+                            initialValue: '121212',
+                            onSaved: (val) {
+                              setState(() {
+                                password = val;
+                              });
+
+                            },
+                            validator: (val) {
+                              if (val.length < 6) {
+                                return '비밀번호는 6글자 이상 12글자 이하로 입력 해주셔야합니다.';
+                              }
+
+                              //return (val as String).isValidPasswordFormatType1() ? null : '비밀번호는 영문(소문자, 대문자), 숫자, 특수문자로 이루어진 6 ~ 12 자리입니다.';
+                              return (val as String).isValidOnlyNumber() ? null : '비밀번호는 숫자로 이루어진 6 ~ 12 자리입니다.';
+                            },
+                          ),
+
+                          const SizedBox(height: 10),
+
+                          // forgot password?
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                TextButton(
+                                  child: const Text("Register now", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold,)),
+                                  onPressed: () {
+                                    Navigator.push(context,
+                                        MaterialPageRoute(builder: (context) {
+                                          return const RegisterPage();
+                                        })
+                                    );
+                                  },
+                                ),
+                                Flexible(
+                                    fit: FlexFit.tight,
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        Text('Forgot Password?', style: TextStyle(color: Colors.grey[600]), ),
+                                      ],
+                                    ),
+                                )
+                              ],
+                            ),
+                          ),
+
+                          const SizedBox(height: 10),
+
+                          // sign in button
+                          loginWidget(context),
+                        ]
+                      )
+                  ),
+                  Flexible(
+                      fit: FlexFit.tight,
+                      flex: 1,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                           padding: const EdgeInsets.symmetric(horizontal: 10.0),
                             child: Row(
                               children: [
                                 Expanded(
@@ -347,6 +374,9 @@ class _LoginPageState extends State<LoginPage> {
                               ],
                             ),
                           ),
+
+                          const SizedBox(height: 10),
+
                           const Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children:  [
@@ -355,27 +385,8 @@ class _LoginPageState extends State<LoginPage> {
                               SquareTile(imagePath: 'lib/images/twitter.png'),
                             ],
                           ),
-
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('Not a member?', style: TextStyle(color: Colors.grey[700]), ),
-                              const SizedBox(width: 4),
-                              TextButton(
-                                child: const Text("Register now", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold,)),
-                                onPressed: () {
-                                  Navigator.push(context,
-                                    MaterialPageRoute(builder: (context) {
-                                      return const RegisterPage();
-                                    })
-                                  );
-                                },
-                              ),
-                            ],
-                          )
                         ],
                       )
-                    ),
                   ),
                 ],
               ),
